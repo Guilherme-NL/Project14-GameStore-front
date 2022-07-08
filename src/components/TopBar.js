02f14d8597ebb17ab11e77c0ec000f8f1b290bf3
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-07-08 17:46:25 -0300

    more typos

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -15,8 +15,8 @@ export default function TopBar(){
             }
         }
         console.log(auth)
-        //const requisition=axios.delete("https://gamemaster-project14.herokuapp.com/session", auth);
-        const requisition=axios.delete("http://localhost:5000/session", auth);
+        const requisition=axios.delete("https://gamemaster-project14.herokuapp.com/session", auth);
+        //const requisition=axios.delete("http://localhost:5000/session", auth);
         requisition.then(response=>{
             navigate('/');
         });
